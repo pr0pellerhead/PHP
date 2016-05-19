3190125dc64f58cc03e88a56c69b1b5222be7214
pr0pellerhead <bojan.gavrovski@gmail.com>
2016-05-19 12:39:57 +0200

    admin gallery manager

---
+++ admin/controllers/galerii.php
@@ -0,0 +1,4 @@
+<?php 
+$query = $db->query('select * from galerii');
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
--- admin/views/galerii.php
+++ admin/views/galerii.php
@@ -1,1 +1,30 @@
 <h3>Galerii</h3>
+<form action="controllers/galerii_add.php" method="post" enctype="multipart/form-data">
+	<input type="text" name="ime" placeholder="gallery name" required>
+	<input type="file" multiple name="sliki[]">
+	<button type="submit">Save</button>
+</form>
+<table border="1" width="80%">
+	<tr>
+		<td>id</td>
+		<td>ime</td>
+		<td>edit</td>
+		<td>delete</td>
+	</tr>
+	<?php foreach($res as $row){ ?>
+	<tr>
+		<td><?=$row['id']?></td>
+		<td><?=$row['ime']?></td>
+		<td>
+			<a href="?page=galerii_edit&id=<?=$row['id']?>">
+				edit
+			</a>
+		</td>
+		<td>
+			<a href="controllers/korisnici_delete.php?id=<?=$row['id']?>">
+				delete
+			</a>
+		</td>
+	</tr>	
+	<?php } ?>
+</table>
